a91c669f5567c49bfba9d0986552bc3e84353bb9
Phuoc Hong Vu <phuocvu511@gmail.com>
2023-12-02 07:05:45 +0700

    fix cors

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 var logger = require('morgan');
+var cors = require('cors');
+
 require('dotenv').config();
 
 var app = express();
+
+const corsOptions = {
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'],
+};
 
 app.use('/upload', express.static('upload'));
 app.use(logger('dev'));
@@ -13,7 +20,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb', parameterLimit: 50000 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors(corsOptions));
 
 // user routers here
 const router = express.Router();
