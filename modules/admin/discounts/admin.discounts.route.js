0a1c6ee53e374bc1338683f916f233e54d53e92b
Phuoc Hong Vu <phuocvu511@gmail.com>
2023-12-04 22:54:31 +0700

    fix discounts

--- modules/admin/discounts/admin.discounts.route.js
+++ modules/admin/discounts/admin.discounts.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/discount/create", DiscountsController.CreateDiscount);
-router.delete("/discount/delete/:id", DiscountsController.DeleteDiscount);
-router.get("/discount/:id", DiscountsController.FindDiscountById);
-router.put("/discount/update", DiscountsController.UpdateDiscount);
-router.get("/discount", DiscountsController.GetAllDiscountsByAdminId);
+router.post("/discounts/create", DiscountsController.CreateDiscount);
+router.delete("/discounts/delete/:id", DiscountsController.DeleteDiscount);
+router.get("/discounts/:id", DiscountsController.FindDiscountById);
+router.put("/discounts/update", DiscountsController.UpdateDiscount);
+router.get("/discounts", DiscountsController.GetAllDiscountsByAdminId);
 
 
 module.exports = router;
