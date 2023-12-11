cec87a4da9bcb5d53bb32de8a8ea12e6b9fd7d60
KiyanoRin <Phuc.nl200475@sis.hust.edu.vn>
2023-12-11 16:56:59 +0700

    =(())=

--- helpers/dbHelper.js
+++ helpers/dbHelper.js
@@ -11,15 +11,27 @@ let dbConnect = () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }
-    console.log('DB connected')  ;
+   
 
     // return mongoose.createConnection('mongodb+srv://jadehillhomestays:1234@cluster0.nwvtu.mongodb.net/jadehillhomestays?retryWrites=true&w=majority',
     //    connectOptions);
-    return mongoose.createConnection('mongodb://localhost:27017/JadeHillHomestays',
-         connectOptions);
+  //  return mongoose.createConnection('mongodb://127.0.0.1:27017/JadeHillHomestays',
+  //       connectOptions);
 
     /* return mongoose.createConnection('mongodb://mongo-jadehills:27017/JadeHillHomestays',
         connectOptions); */
+
+        const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/JadeHillHomestays', connectOptions);
+
+    connection.on('connected', () => {
+        console.log('Connected to MongoDB');
+    });
+
+    connection.on('error', (err) => {
+        console.error('MongoDB connection error:', err);
+    });
+
+    return connection;
 
 }
 exports.db = dbConnect();
