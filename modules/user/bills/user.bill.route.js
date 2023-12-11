1f14b5b65d376c5119679f9edd243cd0e16b71a0
KiyanoRin <Phuc.nl200475@sis.hust.edu.vn>
2023-12-11 08:54:06 +0700

    update user bill

--- modules/user/bills/user.bill.route.js
+++ modules/user/bills/user.bill.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { authUser } = require('../../../middleware');
 
 router.post('/create/bills',BillsController.createBills);
-router.post('/create/bills', authUser ,BillsController.createBillsAuthenticated); 
+router.post('/create/billsAuthenticated', authUser ,BillsController.createBillsAuthenticated); 
 
 module.exports = router;
