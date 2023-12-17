91219dd4079f458f47554c7bd7e064305dbd0ff9
Phuoc Hong Vu <phuocvu511@gmail.com>
2023-12-17 14:28:37 +0700

    update auth

--- modules/admin/bills/admin.bills.route.js
+++ modules/admin/bills/admin.bills.route.js
@@ -2,11 +2,12 @@
 const express = require("express");
 const BillsController = require("./admin.bills.controller");
 const router = express.Router();
+const { authToken } = require('../../../middleware');
 
 // Chức năng trả lại danh sách các bills của admin
-router.get("/bills-of-admin/:id",BillsController.getBillsByAdminId);
+router.get("/bills-of-admin/:id",authToken, BillsController.getBillsByAdminId);
 router.put("/update/bills", BillsController.updateBillsById)
-router.get("/bills-of-homestay",BillsController.getBillsByHomestayId);
-router.delete("/delete/bills/:id", BillsController.deleteBillsById);
+router.get("/bills-of-homestay",authToken, BillsController.getBillsByHomestayId);
+router.delete("/delete/bills/:id", authToken, BillsController.deleteBillsById);
 
 module.exports = router;
